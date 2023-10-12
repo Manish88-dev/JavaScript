6b3f0c3bc98ddf1db125d6e1214482777a24949b
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-12 19:31:04 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" ; // all js code in new version
+
+//alert(4 + 6)  we are using node js not browser 
+
+// data type
+
+//number =123
+//String =""
+//bigint
+//boolean = true/false
+//null
+//undefind 
+//symble = "unque"
+
+console.log(typeof "manish");
+console.log(typeof null);
