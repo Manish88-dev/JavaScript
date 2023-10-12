84c345938c9dde80622a3cb3f837cdc1d87e3436
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-12 19:08:38 +0000

    variable done

---
+++ 01_basic/variable.js
@@ -0,0 +1,17 @@
+const accountID = 18793
+let accountEmail = "manish@gmail.com"
+var accountPassword = 12345
+accountCity = "lucknow"
+
+let accountState;  //undefined
+
+//accountID = 234
+//console.log(accountID);
+
+accountEmail = "ravi@gmail.com"
+accountPassword = 8874
+accountCity = "gorakhpur"
+
+console.log(accountID);
+
+console.table([accountEmail , accountPassword , accountCity , accountState]);
