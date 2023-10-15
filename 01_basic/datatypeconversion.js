76e4f52d80e2b1d0fd3045ba9d178558e6afcb03
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-15 18:29:49 +0000

    conversion

---
+++ 01_basic/datatypeconversion.js
@@ -0,0 +1,19 @@
+//let score = 33
+//let score = 33ans
+//let score = null
+//let score = undefind 
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+
+let somekey = 12
+let stringNumber = String(somekey)
+console.log(stringNumber);
+console.log(typeof stringNumber);
