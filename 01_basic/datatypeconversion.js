d53ac7dcdced3f6fe09591fd87cb8bc943304a3f
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-15 18:49:10 +0000

    Con 2

--- 01_basic/datatypeconversion.js
+++ 01_basic/datatypeconversion.js
@@ -15,5 +15,34 @@
 
 let somekey = 12
 let stringNumber = String(somekey)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/// ********** Operation ******** //
+
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+
+let str1 ="Manish"
+let str2 = "Pandey"
+let str3 = "Ravi"
+
+let str4 = str1+str2+str3
+console.log(str4);
+
+// console.log(1+"2");
+// console.log("2"+1);
+// console.log(1+2+"2");
+// console.log("1"+(2+2));
+// console.log("1"+2+3);
+
+console.log(+true);
+console.log(true);
