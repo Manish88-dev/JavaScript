551f721305bfd15a503bbd46891d0181208c7371
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-16 09:39:56 +0000

    summary datatypes

---
+++ 01_basic/datatypesummary.js
@@ -0,0 +1,31 @@
+// 1 -- Basically there are two datatypes
+// 1 Primitive  (7 cateagry)
+//1 - string ,Number,boolean, null,undefind,symbol,bigInt
+
+const score = 100
+const scoreValue =100.3
+
+const boolean = false
+const outside = null
+let accountEmail;
+
+const bigInt = 323434336456654n
+
+//Reference(non primitive)
+
+//array , object, fuction
+
+// JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution
+
+const Heros = ["JuniorG" , "Saktiman" , "tobo" ];
+
+
+let muobj ={
+    name: "manish",
+    age :22,
+}
+
+const myfunction = function(){
+    console.log("hello Manish");
+}
+myfunction();
