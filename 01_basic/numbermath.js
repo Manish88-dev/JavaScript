03e778815332d6ab24c74847a2651689c3eda89f
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-18 18:46:09 +0000

    numbermath

---
+++ 01_basic/numbermath.js
@@ -0,0 +1,24 @@
+const score = 200
+console.log(score);
+const paisa = new Number(200);
+// console.log(paisa);
+// console.log(paisa.toString().length);
+// console.log(paisa.toFixed(2));
+
+const othernumber = 12.343
+console.log(othernumber.toPrecision(3));
+
+//*******  ----Math -- ********* *//
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(2.2));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
