4c143eadad0093499157678e105951daa5df5233
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-18 19:12:48 +0000

    date

---
+++ 01_basic/datesinjs.js
@@ -0,0 +1,9 @@
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleString());
+ console.log(typeof mydate);
+ console.log(mydate.getDay());
+
+
