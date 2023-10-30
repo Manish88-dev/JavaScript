16e9a7ffcd48a05bd906010d07a7bd6cd78601b1
Manish Pandey <76435527+Manish88-dev@users.noreply.github.com>
2023-10-30 18:56:07 +0000

    scopes

---
+++ 03_basic/scopes.js
@@ -0,0 +1,19 @@
+//{} this is also know as scopes , jb ye fuction or if else ka sath ata hai 
+
+// let a = 9
+// const b = 10
+// var c = 40
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//but this way 
+if (true) {
+    let a = 9
+    const b = 10
+    var c = 40
+}
+//console.log(a); //a is not defined
+//console.log(b);// b is not defined
+console.log(c); //output 40 
